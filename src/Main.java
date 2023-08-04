77cb622969389a01b1e95c96a7937368785bfbba
phaydara <stephen.mussel@gmail.com>
2023-08-04 14:38:07 -0500

    captured input for principal

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+//        TODO: take input for principal
+//        TODO: take input for annual interest rate (%)
+//        TODO: take input for payment period in years
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.println(principal);
     }
 }
