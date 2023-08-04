edf8b17555cce3d07240264f86aa038aa4d3be10
phaydara <stephen.mussel@gmail.com>
2023-08-04 14:58:50 -0500

    added constants to interest rate and terms conversions

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        final int PERCENT = 100;
+        final int MONTHS_IN_YEARS = 12;
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Principal: ");
@@ -11,11 +14,11 @@ public class Main {
 
         System.out.println("Annual Interest Rate: ");
         float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / 100 / 12;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
 
         System.out.println("Payment Terms (years): ");
         byte years = scanner.nextByte();
-        int months = years * 12;
+        int months = years * MONTHS_IN_YEARS;
 
         double mortgage = principal
                 * (monthlyInterest * (Math.pow(1 + monthlyInterest, months)))
