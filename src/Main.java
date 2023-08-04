c9bdeff07c609c148d167c99b2f6a970d225e7cd
phaydara <stephen.mussel@gmail.com>
2023-08-04 14:51:16 -0500

    calculated monthly mortgage payment

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 //        DONE: take input for principal
 //        DONE: take input for annual interest rate (%)
 //        DONE: take input for payment terms in years
+//        TODO: calculate mortgage
+//        DESC: mortgage = principal(monthlyInterest(1 + monthlyInterest)^terms/((1 + monthlyInterest)^terms) - 1))
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,5 +22,11 @@ public class Main {
         byte years = scanner.nextByte();
         int months = years * 12;
         System.out.println(months);
+
+        double mortgage = principal
+                * (monthlyInterest * (Math.pow(1 + monthlyInterest, months)))
+                / (Math.pow(1 + monthlyInterest, months) -1);
+
+        System.out.println("Monthly Mortgage Payment: " + mortgage);
     }
 }
