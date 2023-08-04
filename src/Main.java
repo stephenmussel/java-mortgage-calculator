9d00c1cbecc4b5c22a632ecede6a0efb591ac711
phaydara <stephen.mussel@gmail.com>
2023-08-04 15:30:27 -0500

    validated inputs for annual interest rate and payment terms

--- src/Main.java
+++ src/Main.java
@@ -12,34 +12,43 @@ public class Main {
         final int PERCENT = 100;
         final int MONTHS_IN_YEARS = 12;
         int principal = 0;
-
+        float monthlyInterest = 0;
+        int months = 0;
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Principal: ");
-             principal = scanner.nextInt();
+            principal = scanner.nextInt();
             if (principal >= 1_000 && principal <= 1_000_000) {
                 break;
             }
             System.out.println("Principal must be between 1,000 and 1,000,000");
         }
 
-//        NOTES: if input is between 1 and 30 continue to payment terms
-//        NOTES: if input is invalid print reminder and try input again
-        System.out.println("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEARS;
+                break;
+            }
+            System.out.println("Annual Interest Rate must be between 1 and 30");
+        }
 
-//        NOTES: if input is between 1 and 30 continue, calculate mortgage
-//        NOTES: if input is invalid print reminder and try input again
-        System.out.println("Payment Terms (years): ");
-        byte years = scanner.nextByte();
-        int months = years * MONTHS_IN_YEARS;
+        while (true) {
+            System.out.println("Payment Terms (years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                months = years * MONTHS_IN_YEARS;
+                break;
+            }
+            System.out.println("Payment terms (years) must be between 1 and 30");
+        }
 
         double mortgage = principal
                 * (monthlyInterest * (Math.pow(1 + monthlyInterest, months)))
-                / (Math.pow(1 + monthlyInterest, months) -1);
+                / (Math.pow(1 + monthlyInterest, months) - 1);
         String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
 
         System.out.println("Monthly Mortgage Payment: " + formattedMortgage);
