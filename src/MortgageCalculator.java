80392fc4bffbbd65d01c412f942aad455f5b0641
phaydara <stephen.mussel@gmail.com>
2023-08-14 15:10:43 -0500

    moved parameters to fields and added constructor

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -1,4 +1,15 @@
 public class MortgageCalculator {
+
+    private int principal;
+    private float annualInterest;
+    private byte years;
+
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this.principal = principal;
+        this.annualInterest = annualInterest;
+        this.years = years;
+    }
+
     public static double calculateBalance(int principal, float annualInterest, byte years, int numberOfPaymentsMade) {
 
         float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
