9944aa827be37ec41ea7fbb43a1f654ad77ab457
phaydara <stephen.mussel@gmail.com>
2023-08-14 16:13:51 -0500

    refactored currency formatter

--- src/MortgageReport.java
+++ src/MortgageReport.java
@@ -2,10 +2,12 @@ import java.text.NumberFormat;
 
 public class MortgageReport {
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -14,15 +16,14 @@ public class MortgageReport {
         System.out.println("----------------");
 
         for (double balances : calculator.getRemainingBalances()) {
-            String formattedNewBalance = NumberFormat.getCurrencyInstance().format(balances);
-            System.out.println(formattedNewBalance);
+            System.out.println(currency.format(balances));
         }
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
 
-        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+        String formattedMortgage = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
