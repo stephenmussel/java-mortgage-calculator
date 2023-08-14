b5c4247a27789152ee62b9fa5e68fcc08c55978f
phaydara <stephen.mussel@gmail.com>
2023-08-14 15:21:38 -0500

    removed parameters from calculateMortgage method

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -22,7 +22,7 @@ public class MortgageCalculator {
         return balance;
     }
 
-    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+    public double calculateMortgage() {
 
         float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
         int months = years * Main.MONTHS_IN_YEAR;
